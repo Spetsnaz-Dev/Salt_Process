4cfb8e309ef5236e5b69d7b4b09cbd6c1be8eeb4
Spetsnaz-Dev <ravindrafk@gmail.com>
2020-02-17 02:11:55 +0530

    initial

---
+++ app.py
@@ -0,0 +1,125 @@
+# web-app for API image manipulation
+
+from flask import Flask, request, render_template, send_from_directory
+import os
+import image
+from flask import Flask, request, jsonify, render_template
+import numpy as np 
+import tensorflow as tf
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+from tensorflow.keras.models import load_model
+import numpy as np
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def get_iou_vector(A, B):
+    batch_size = A.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        t, p = A[batch]>0, B[batch]>0
+#         if np.count_nonzero(t) == 0 and np.count_nonzero(p) > 0:
+#             metric.append(0)
+#             continue
+#         if np.count_nonzero(t) >= 1 and np.count_nonzero(p) == 0:
+#             metric.append(0)
+#             continue
+#         if np.count_nonzero(t) == 0 and np.count_nonzero(p) == 0:
+#             metric.append(1)
+#             continue
+        
+        intersection = np.logical_and(t, p)
+        union = np.logical_or(t, p)
+        iou = (np.sum(intersection > 0) + 1e-10 )/ (np.sum(union > 0) + 1e-10)
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric.append(np.mean(s))
+
+    return np.mean(metric)
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred>0.5], tf.float64)
+
+model = load_model(r"C:\Users\LENOVO\Downloads\U-net.h5", custom_objects={'my_iou_metric':my_iou_metric})
+model._make_predict_function()
+
+
+# default access page
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+# upload selected image and forward to processing page
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'static/images/')
+
+    # create image directory if not found
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    # retrieve file from html file-picker
+    upload = request.files.getlist("file")[0]
+    print("File name: {}".format(upload.filename))
+    filename = upload.filename
+
+    # file support verification
+    ext = os.path.splitext(filename)[1]
+    if (ext == ".jpg") or (ext == ".png") or (ext == ".bmp"):
+        print("File accepted")
+    else:
+        return render_template("error.html", message="The selected file is not supported"), 400
+
+    # save file
+    destination = "/".join([target, filename])
+    print("File saved to to:", destination)
+    upload.save(destination)
+
+    # forward to processing page
+    return render_template("processing.html", image_name=filename)
+
+
+# rotate filename the specified degrees
+@app.route("/rotate", methods=["POST"])
+def rotate():
+
+    # open and process image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = image.open(destination)
+    image_arr = img_to_array(img)
+
+
+    x = np.expand_dims(image_arr, axis=0)
+
+    preds = model.predict(x)
+    preds = preds.reshape(101,101,1)
+
+    img = array_to_img(preds)
+
+
+    # save and return image
+    destination = "/".join([target, 'temp.png'])
+    if os.path.isfile(destination):
+        os.remove(destination)
+    img.save(destination)
+
+    return send_image('temp.png')
+
+
+
+
+# retrieve file from 'static/images' directory
+@app.route('/static/images/<filename>')
+def send_image(filename):
+    return send_from_directory("static/images", filename)
+
+
+if __name__ == "__main__":
+    app.run()
+
